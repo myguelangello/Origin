6973acf88e014ed5a7f52a06675f6aa736b1b2dd
myguel_angello <myguelangello624@gmail.com>
2021-09-06 09:09:23 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+/* abre/fecha menu */
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+}
+
+/* handle links menu */
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
+}
+
+/* mudar header quando der scrool */
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
+
+  if (window.scrollY >= navHeight) {
+    /* scroll é maior que a altura do header */
+    header.classList.add("scroll");
+  } else {
+    /* menor que a altura do header */
+    header.classList.remove("scroll");
+  }
+}
+
+/* Testimonials carousel */
+const swiper = new Swiper(".swiper-container", {
+  /* add */ loop: true,
+  slidesPerwiew: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text, 
+  #about .image, #about .text, 
+  #services header, #services .card, 
+  #testimonials header, #testimonials .testimonials, 
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+);
+
+/* Back to top button */
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
